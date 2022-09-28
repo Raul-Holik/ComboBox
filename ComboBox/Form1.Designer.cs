d1fcb6a4fa412bf66c920c9a73e5d2d2232d3c69
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-09-28 09:30:40 +0200

    Add files via upload

---
+++ ComboBox/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.txtBx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "IT menadzment",
+            "Aplikativno programiranje",
+            "Inernet programiraje",
+            "Informacioni sistemi"});
+            this.comboBox.Location = new System.Drawing.Point(182, 99);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // txtBx
+            // 
+            this.txtBx.Location = new System.Drawing.Point(182, 253);
+            this.txtBx.Name = "txtBx";
+            this.txtBx.Size = new System.Drawing.Size(142, 20);
+            this.txtBx.TabIndex = 1;
+            this.txtBx.TextChanged += new System.EventHandler(this.txtBx_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBx);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.TextBox txtBx;
+    }
+}
+
